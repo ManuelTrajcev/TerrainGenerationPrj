67f2e0276cbd123470bd537f96f6f518bff2e942
ManuelTrajcev <manueltrajcev7@gmail.com>
2024-08-05 16:41:07 +0200

    Terrain 1 - Cleared

--- Terrain1/terrain.h
+++ Terrain1/terrain.h
@@ -8,29 +8,6 @@
 #include "terrain_technique.h"
 
 
-class BaseTerrain
-{
-public:
-	BaseTerrain() {}
 
-	void InitTerrain(float WorldScale);
-
-	void Render(const BasicCamera& Camera);
-
-	void LoadFromFile(const char* pFilename);
-
-	float GetHeight(int x, int z) const { return m_heightMap.Get(x, z); }
-
-	float GetWorldScale() const { return m_worldScale; }
-
-protected:
-	void LoadHeightMapFile(const char* pFilename);
-
-	int m_terrainSize = 0;
-	float m_worldScale = 1.0f;
-	Array2D<float> m_heightMap;
-	TriangleList m_triangleList;
-	TerrainTechnique m_terrainTech;
-};
 
 #endif
--- Terrain1/triangle_list.h
+++ Terrain1/triangle_list.h
@@ -10,33 +10,6 @@
 // declaration for BaseTerrain.
 class BaseTerrain;
 
-class TriangleList {
-public:
-	TriangleList();
 
-	void CreateTriangleList(int Width, int Depth, const BaseTerrain* pTerrain);
-
-	void Render();
-
-private:
-
-	struct Vertex {
-		Vector3f Pos;
-
-		void InitVertex(const BaseTerrain* pTerrain, int x, int z);
-	};
-
-	void CreateGLState();
-
-	void PopulateBuffers(const BaseTerrain* pTerrain);
-	void InitVertices(const BaseTerrain* pTerrain, std::vector<Vertex>& Vertices);
-	void InitIndices(std::vector<uint>& Indices);
-
-	int m_width = 0;
-	int m_depth = 0;
-	GLuint m_vao;
-	GLuint m_vb;
-	GLuint m_ib;
-};
 
 #endif
