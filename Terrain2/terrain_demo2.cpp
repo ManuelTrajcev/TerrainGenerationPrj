4ee58950731e2161d334a2e1dde210a57534fac6
ManuelTrajcev <manueltrajcev7@gmail.com>
2024-08-06 11:52:40 +0200

    Terrain 2 - Cleared (FaultFormation algortihm)

--- Terrain2/terrain_demo2.cpp
+++ Terrain2/terrain_demo2.cpp
@@ -1,23 +1,3 @@
-/*
-
-        Copyright 2022 Etay Meiri
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    Terrain Rendering - demo 2
-*/
-
 #ifdef _WIN64 // until I install imgui on Linux...
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
@@ -221,14 +201,9 @@ private:
     void InitTerrain()
     {
         float WorldScale = 4.0f;
-        m_terrain.InitTerrain(WorldScale);
+    
 
-        int Size = 256;
-        int Iterations = 500;
-        float MinHeight = 0.0f;
-        float MaxHeight = 300.0f;
-        float Filter = 0.5f;
-        m_terrain.CreateFaultFormation(Size, Iterations, MinHeight, MaxHeight, Filter);
+       
     }
 
 
@@ -255,8 +230,7 @@ private:
     GLFWwindow* window = NULL;
     BasicCamera* m_pGameCamera = NULL;
     bool m_isWireframe = false;
-    FaultFormationTerrain m_terrain;
-    bool m_showGui = false;
+ 
 };
 
 TerrainDemo2* app = NULL;
