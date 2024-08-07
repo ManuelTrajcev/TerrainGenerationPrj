cfa5cdf1a2b636db12298cad272da797878f1923
ManuelTrajcev <manueltrajcev7@gmail.com>
2024-08-07 17:16:57 +0200

    Terrain 6 cleared

--- Terrain6/terrain_demo6.cpp
+++ Terrain6/terrain_demo6.cpp
@@ -1,22 +1,3 @@
-/*
-
-        Copyright 2022 Etay Meiri
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    Terrain Rendering - demo 6 - Geomipmapping
-*/
 
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
@@ -258,7 +239,7 @@ private:
 
         m_terrain.InitTerrain(WorldScale, TextureScale, TextureFilenames);
 
-        m_terrain.CreateMidpointDisplacement(m_terrainSize, m_patchSize, m_roughness, m_minHeight, m_maxHeight);
+       //
 
         Vector3f LightDir(1.0f, -1.0f, 0.0f);
 
