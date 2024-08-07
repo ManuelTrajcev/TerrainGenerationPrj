ef9cac62925f18067ee832f3b7c5dcf3d739beb2
ManuelTrajcev <manueltrajcev7@gmail.com>
2024-08-07 18:00:06 +0200

    problem of cracks solved (2 различни LOD кои се соседни)

--- Terrain7/geomip_grid.cpp
+++ Terrain7/geomip_grid.cpp
@@ -1,22 +1,3 @@
-/*
-
-        Copyright 2023 Etay Meiri
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-
 #include <stdio.h>
 #include <vector>
 
@@ -387,7 +368,7 @@ void GeomipGrid::Render(const Vector3f& CameraPos)
     if (gShowPoints != 2) {
         for (int PatchZ = 0 ; PatchZ < m_numPatchesZ ; PatchZ++) {
             for (int PatchX = 0 ; PatchX < m_numPatchesX ; PatchX++) {
-                const LodManager::PatchLod& plod = m_lodManager.GetPatchLod(PatchX, PatchZ);
+                const LodManager::PatchLod& plod = m_lodManager.GetPatchLod(PatchX, PatchZ);    //Vo zavisnost od lokacijata vo grid se opredeluva koj LOD se koristi
                 int C = plod.Core;
                 int L = plod.Left;
                 int R = plod.Right;
--- Terrain7/geomip_grid.h
+++ Terrain7/geomip_grid.h
@@ -8,6 +8,77 @@
 #include "ogldev_math_3d.h"
 #include "lod_manager.h"
 
+class BaseTerrain;
+
+class GeomipGrid {
+public:
+    GeomipGrid();
+
+    ~GeomipGrid();
+
+    void CreateGeomipGrid(int Width, int Depth, int PatchSize, const BaseTerrain* pTerrain);
+
+    void Destroy();
+
+    void Render(const Vector3f& CameraPos);
+
+private:
+
+    struct Vertex {
+        Vector3f Pos;
+        Vector2f Tex;
+        Vector3f Normal = Vector3f(0.0f, 0.0f, 0.0f);
+
+        void InitVertex(const BaseTerrain* pTerrain, int x, int z);
+    };
+
+    void CreateGLState();
+
+    void PopulateBuffers(const BaseTerrain* pTerrain);
+
+    void InitVertices(const BaseTerrain* pTerrain, std::vector<Vertex>& Vertices);
+
+    int InitIndices(std::vector<uint>& Indices);
+
+    int InitIndicesLOD(int Index, std::vector<uint>& Indices, int lod);
+
+    int InitIndicesLODSingle(int Index, std::vector<uint>& Indices, int lodCore, int lodLeft, int lodRight, int lodTop, int lodBottom);
+
+    void CalcNormals(std::vector<Vertex>& Vertices, std::vector<uint>& Indices);
+
+    uint AddTriangle(uint Index, std::vector<uint>& Indices, uint v1, uint v2, uint v3);
+
+    uint CreateTriangleFan(int Index, std::vector<uint>& Indices, int lodCore, int lodLeft, int lodRight, int lodTop, int lodBottom, int x, int z);
+
+    int CalcNumIndices();
+
+    int m_width = 0;
+    int m_depth = 0;
+    int m_patchSize = 0;
+    int m_maxLOD = 0;       //Se opredeluva vo LOD menager
+    GLuint m_vao = 0;
+    GLuint m_vb = 0;
+    GLuint m_ib = 0;
+
+    struct SingleLodInfo {
+        int Start = 0;
+        int Count = 0;
+    };
+
+#define LEFT   2
+#define RIGHT  2
+#define TOP    2
+#define BOTTOM 2
+
+    struct LodInfo {
+        SingleLodInfo info[LEFT][RIGHT][TOP][BOTTOM];
+    };
+
+    std::vector<LodInfo> m_lodInfo;
+    int m_numPatchesX = 0;
+    int m_numPatchesZ = 0;
+    LodManager m_lodManager;
+};
 
 
 #endif
