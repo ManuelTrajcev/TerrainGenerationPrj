f2c4f134cd32359d26102e6b9b77baaa7eaef27a
ManuelTrajcev <manueltrajcev7@gmail.com>
2024-08-09 12:59:34 +0200

    QuadList class

--- Terrain13/quad_list.h
+++ Terrain13/quad_list.h
@@ -9,3 +9,29 @@
 class BaseTerrain;
 
 
+class QuadList {
+public:
+    QuadList();
+    ~QuadList();
+    void CreateQuadList(int Width, int Depth, const BaseTerrain* pTerrain);
+    void Destroy();
+    void Render();
+private:
+    struct Vertex {
+        Vector3f Pos;
+        Vector2f Tex;
+        void InitVertex(const BaseTerrain* pTerrain, int Width, int Depth, int x, int z);
+    };
+
+    void CreateGLState();
+    void PopulateBuffers(const BaseTerrain* pTerrain);
+    void InitVertices(const BaseTerrain* pTerrain, std::vector<Vertex>& Vertices);
+    void InitIndices(std::vector<uint>& Indices);
+
+    int m_width = 0;
+    int m_depth = 0;
+
+    GLuint m_vao = 0;
+    GLuint m_vb = 0;
+    GLuint m_ib = 0;
+};
